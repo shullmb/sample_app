e33c38d7fd1ba2e3ba367b8ec2f2744c97c248d2
shullmb <dev.shullmb@gmail.com>
2015-12-17 16:29:43 -0800

    add email validations and unit testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,29 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "#{'s'*244}@example.com"
     assert_not @user.valid?
   end
+
+  test "email should accept valid email addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email should not accept invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user@example. foo@bar_baz.com foo@bar+baz.com]
+
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
