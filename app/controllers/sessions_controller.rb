8d2722b07019ebdeae47baa51c6cdf88238a6e1e
shullmb <dev.shullmb@gmail.com>
2015-12-27 10:01:47 -0800

    integration test for invalid login credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login and redirect to show page
+    else
+      flash.now[:danger] = "Oops, looks like a typo! This email/password combo is invalid"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
