f3dee851324fb234adcafc66c37fd008f4e1d24f
shullmb <dev.shullmb@gmail.com>
2015-12-30 18:45:03 -0800

    add login & integration testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # login and redirect to show page
+      log_in(user)
+      redirect_to user
     else
       flash.now[:danger] = "Oops, looks like a typo! This email/password combo is invalid"
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+
+  # login a given user
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # returns current session user if logged in
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
