f586ab8a842f6be477b10a01814432e32f8da0f1
shullmb <dev.shullmb@gmail.com>
2016-01-03 13:03:26 -0800

    fix friendly forwarding test, create user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_as, only: [ :edit, :update ]
+  before_action :logged_in_user, only: [ :index, :edit, :update ]
   before_action :correct_user, only: [ :edit, :update ]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -44,8 +48,9 @@ class UsersController < ApplicationController
     # before filters
 
     # confirms logged in User
-    def logged_in_as
+    def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in"
         redirect_to login_url
       end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,4 +49,15 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # redirects to the stored url (or to default)
+  def redirect_back_or default
+    redirect_to( session[:forwarding_url] || default )
+    session.delete(:forwarding_url)
+  end
+
+  # stores the requested url
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end
