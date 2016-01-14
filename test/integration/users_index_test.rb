99dba9ced67041cb34f81caf01049f50fe1b392f
shullmb <dev.shullmb@gmail.com>
2016-01-13 18:26:26 -0800

    refactor user index with partial

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,7 +7,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   end
 
   test "index including pagination" do
-    log_in_as @user
+    log_in_as(@user)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
